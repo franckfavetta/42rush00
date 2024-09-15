cdfaa3cfbde122d0f5217b42aa840aa682e30c25
Yves Gille <ygille@student.42lyon.fr>
2024-09-15 11:15:27 +0200

    correctedAtoi

--- yves/ex00exec/testatoi.c
+++ yves/ex00exec/testatoi.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testatoi.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygille <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 11:05:24 by ygille            #+#    #+#             */
+/*   Updated: 2024/09/15 11:14:58 by ygille           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	satoi(char *str)
 {
 	int sign;
@@ -6,6 +18,7 @@ int	satoi(char *str)
 	
 	pos = 0;
 	sign = 1;
+	num = 0;
 	if (str[pos] == '-')
 	{
 		sign = -1;
@@ -14,10 +27,21 @@ int	satoi(char *str)
 	while (str[pos])
 	{
 		num = num + (str[pos] - 48);
-		if (str[pos++])
+		if (str[pos + 1])
 			num *=10;
 		pos++;
 	}
 	num *= sign;
 	return (num);
+}
+
+int	main(void)
+{
+	char	a[] = "-123";
+	int	b;
+
+	printf("%s\n",a);
+	b = satoi(a);
+	printf("%d",b);
+	return (0);
 }
